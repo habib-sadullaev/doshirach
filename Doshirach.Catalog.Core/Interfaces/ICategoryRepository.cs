749a95184f78ac8d6d215a870c219af4452f6252
habib sadullaev <h.sadullaev@gmail.com>
2023-05-02 17:14:14 +0500

    implement catalog repository interfaces

---
+++ Doshirach.Catalog.Core/Interfaces/ICategoryRepository.cs
@@ -0,0 +1,12 @@
+﻿using Doshirach.Catalog.Core.Models;
+
+namespace Doshirach.Catalog.Core.Interfaces;
+
+public interface ICategoryRepository
+{
+	Category Get(int id);
+	List<Category> List();
+	void Add(Category category);
+	void Update(Category category);
+	void Delete(int id);
+}
---
+++ Doshirach.Catalog.Core/Interfaces/IItemRepository.cs
@@ -0,0 +1,12 @@
+﻿using Doshirach.Catalog.Core.Models;
+
+namespace Doshirach.Catalog.Core.Interfaces;
+
+public interface IItemRepository
+{
+	Item Get(int id);
+	List<Item> List();
+	void Add(Item item);
+	void Update(Item item);
+	void Delete(int id);
+}
