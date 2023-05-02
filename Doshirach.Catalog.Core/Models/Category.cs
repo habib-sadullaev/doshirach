46db1783314ac029022ac5574375facf24e190d9
habib sadullaev <h.sadullaev@gmail.com>
2023-05-02 17:11:59 +0500

    implement catalog models

---
+++ Doshirach.Catalog.Core/Models/Category.cs
@@ -0,0 +1,9 @@
+﻿namespace Doshirach.Catalog.Core.Models;
+
+public class Category
+{
+	public required int Id { get; init; }
+	public required string Name { get; init; }
+	public string? Image { get; set; }
+	public int? ParentCategoryId { get; set; }
+}
---
+++ Doshirach.Catalog.Core/Models/Item.cs
@@ -0,0 +1,12 @@
+﻿namespace Doshirach.Catalog.Core.Models;
+
+public class Item
+{
+	public required int Id { get; init; }
+	public required string Name { get; init; }
+	public string? Description { get; set; }
+	public string? Image { get; set; }
+	public required int CategoryId { get; set; }
+	public required decimal Price { get; set; }
+	public required int Amount { get; set; }
+}
