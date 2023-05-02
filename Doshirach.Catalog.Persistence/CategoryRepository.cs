498e22f69417d90cadca48060238293e82dd07dc
habib sadullaev <h.sadullaev@gmail.com>
2023-05-02 18:21:27 +0500

    implement persistance layer for Catalog

---
+++ Doshirach.Catalog.Persistence/CategoryRepository.cs
@@ -0,0 +1,46 @@
+﻿using Dapper;
+using Doshirach.Catalog.Core.Interfaces;
+using Doshirach.Catalog.Core.Models;
+using Microsoft.Data.SqlClient;
+
+namespace Doshirach.Catalog.Persistence;
+
+public class CategoryRepository : ICategoryRepository
+{
+	private readonly string connectionString;
+
+	public CategoryRepository(string connectionString)
+	{
+		this.connectionString = connectionString;
+	}
+
+	public Category Get(int id)
+	{
+		using var conn = new SqlConnection(connectionString);
+		return conn.QueryFirstOrDefault<Category>("SELECT * FROM Category WHERE id = @id", new { id });
+	}
+
+	public List<Category> List()
+	{
+		using var conn = new SqlConnection(connectionString);
+		return conn.Query<Category>("SELECT * FROM Category").ToList();
+	}
+
+	public void Add(Category category)
+	{
+		using var conn = new SqlConnection(connectionString);
+		conn.Execute("INSERT INTO Category (name, image, parent_category_id) VALUES (@Name, @Image, @ParentCategoryId)", category);
+	}
+
+	public void Update(Category category)
+	{
+		using var conn = new SqlConnection(connectionString);
+		conn.Execute("UPDATE Category SET name = @Name, image = @Image, parent_category_id = @ParentCategoryId WHERE id = @Id", category);
+	}
+
+	public void Delete(int id)
+	{
+		using var conn = new SqlConnection(connectionString);
+		conn.Execute("DELETE FROM Category WHERE id = @id", new { id });
+	}
+}
---
+++ Doshirach.Catalog.Persistence/ItemRepository.cs
@@ -0,0 +1,54 @@
+﻿using Dapper;
+using Doshirach.Catalog.Core.Interfaces;
+using Doshirach.Catalog.Core.Models;
+using Microsoft.Data.SqlClient;
+
+namespace Doshirach.Catalog.Persistence;
+
+public class ItemRepository : IItemRepository
+{
+	private readonly string connectionString;
+
+	public ItemRepository(string connectionString)
+	{
+		this.connectionString = connectionString;
+	}
+
+	public Item Get(int id)
+	{
+		using var conn = new SqlConnection(connectionString);
+		return conn.QueryFirstOrDefault<Item>("SELECT * FROM Item WHERE id = @id", new { id });
+	}
+
+	public List<Item> List()
+	{
+		using var conn = new SqlConnection(connectionString);
+		return conn.Query<Item>("SELECT * FROM Item").ToList();
+	}
+
+	public void Add(Item item)
+	{
+		using var conn = new SqlConnection(connectionString);
+		conn.Execute("INSERT INTO Item (name, description, image, category_id, price, amount) VALUES (@Name, @Description, @Image, @CategoryId, @Price, @Amount)", item);
+	}
+
+	public void Update(Item item)
+	{
+		using var conn = new SqlConnection(connectionString);
+		conn.Execute(
+			@"""UPDATE Item SET 
+						 name = @Name
+						,description = @Description
+						,image = @Image
+						,category_id = @CategoryId
+						,price = @Price
+						,amount = @Amount 
+					WHERE id = @Id""", item);
+	}
+
+	public void Delete(int id)
+	{
+		using var conn = new SqlConnection(connectionString);
+		conn.Execute("DELETE FROM Item WHERE id = @id", new { id });
+	}
+}
