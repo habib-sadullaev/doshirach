507aefe76fc5625320cb7a1c5979c2ffa8f10026
habib sadullaev <h.sadullaev@gmail.com>
2023-06-11 16:13:45 +0500

    add identity server

---
+++ Doshirach.IdentityServer/Config.cs
@@ -0,0 +1,54 @@
+﻿using Duende.IdentityServer.Models;
+
+namespace Doshirach.IdentityServer
+{
+	public static class Config
+	{
+		public static IEnumerable<IdentityResource> IdentityResources =>
+			 new IdentityResource[]
+			 {
+				new IdentityResources.OpenId(),
+				new IdentityResources.Profile(),
+			 };
+
+		public static IEnumerable<ApiScope> ApiScopes =>
+			 new ApiScope[]
+			 {
+				new ApiScope("appScope"),
+			 };
+
+		public static IEnumerable<Client> Clients =>
+			 new Client[]
+			 {
+            new Client
+				{
+					 ClientId = "catalog",
+					 ClientSecrets = { new Secret("49C1A7E1-0C79-4A89-A3D6-A37998FB86B0".Sha256()) },
+
+					 AllowedGrantTypes = GrantTypes.Code,
+
+					 RedirectUris = { "https://localhost:5242/signin-oidc" },
+					 FrontChannelLogoutUri = "https://localhost:5242/signout-oidc",
+					 PostLogoutRedirectUris = { "https://localhost:5242/signout-callback-oidc" },
+
+					 AllowOfflineAccess = true,
+					 AllowedScopes = { "openid", "profile", "appScope" }
+				},
+
+				new Client
+				{
+					 ClientId = "carting",
+					 ClientSecrets = { new Secret("49C1A7E1-0C79-4A89-A3D6-A37998FB86B0".Sha256()) },
+
+					 AllowedGrantTypes = GrantTypes.Code,
+
+					 RedirectUris = { "https://localhost:5005/signin-oidc" },
+					 FrontChannelLogoutUri = "https://localhost:5005/signout-oidc",
+					 PostLogoutRedirectUris = { "https://localhost:5005/signout-callback-oidc" },
+
+					 AllowOfflineAccess = true,
+					 AllowedScopes = { "openid", "profile", "appScope" }
+				},
+			 };
+	}
+}
